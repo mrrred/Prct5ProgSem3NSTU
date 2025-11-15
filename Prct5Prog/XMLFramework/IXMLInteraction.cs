4b726c5f26136a74340da264a9714da9bc3707bd
mrrred <zarubinr85@gmail.com>
2025-11-15 19:12:16 +0700

    Interface v1

---
+++ Prct5Prog/XMLFramework/IXMLInteraction.cs
@@ -0,0 +1,21 @@
+﻿using BoolMatrixFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prct5Prog.XMLFramework
+{
+    interface IXMLInteraction
+    {
+        void Add(BoolMatrix boolMatrix);
+
+        BoolMatrix Pop(int id);
+
+        int Search(BoolMatrix boolMatrix);
+
+        void EditElement(int id, BoolMatrix boolMatrix);
+
+    }
+}
